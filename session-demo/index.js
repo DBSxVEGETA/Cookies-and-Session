7ec3e965e4024ecee0fba6278dae51dcfd8aab7e
Apurva Kumar <apurvakumar0313@gmail.com>
2024-01-24 00:46:23 +0530

    Learning express-session

--- session-demo/index.js
+++ session-demo/index.js
@@ -1,16 +1,46 @@
-const express = require("express");
-const session = require("express-session");
+const express = require('express');
 const app = express();
+const session = require('express-session');
 
-app.get("/viewcount", (req, res) => {
-  if (req.session.viewcount) {
-    req.session.viewcount += 1;
-  } else {
-    req.session.viewcount = 1;
-  }
-  res.send(`You are viewing this page ${req.session.viewcount} times`);
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, maxAge: 60000 }
+}))
+
+
+app.get('/', (req, res) => {
+    res.send('Hello Session')
+})
+
+app.get('/greet', (req, res) => {
+    const { username } = req.query;
+    console.log(req.query);
+    if (username) {
+        req.session.username = username;
+    }
+    res.send(`Hello ${username}`)
 });
 
-app.listen("3000", () => {
-  console.log("Express server started at port 3000");
-});
+app.get('/get-username', (req, res) => {
+    const { username = 'Anonymous' } = req.session;
+    res.send(`Your username is ${username}`)
+})
+
+app.get('/viewcount', (req, res) => {
+    if (req.session.viewcount) {
+        req.session.viewcount += 1;
+    }
+    else {
+        req.session.viewcount = 1;
+    }
+    res.send(`You have viewed this page ${req.session.viewcount} times`)
+})
+
+
+app.listen(5000, () => {
+    console.log('Express server started at port 5000');
+
+})
